595065119cdaa745c844ce77da889d17ae85f434
sidak-245 <74825336+sidak-245@users.noreply.github.com>
2021-04-07 19:59:12 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,24 @@
+class Bird extends BaseClass {
+  constructor(x, y) {
+    super(x, y, 50, 50);
+    this.image = loadImage("sprites/bird.png");
+    this.smokeimage = loadImage("sprites/smoke.png")
+    this.path = []
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+    if (this.body.position.x < 220) {
+      var position = [this.body.position.x, this.body.position.y]
+      this.path.push(position);
+     
+    }
+    console.log(this.path)
+    for(var i =0; i<this.path.length;i++){
+image (this.smokeimage,this.path[i][0],this.path[i][1])
+    }
+    
+    super.display();
+  }
+}
